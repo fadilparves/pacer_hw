35114a743d47ffd48e78f586be4e7459a555c712
Mohamad Fadil <fadil.parves@gmail.com>
2023-06-06 15:10:03 +0800

    added the first model

--- pacer_app/models.py
+++ pacer_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Score(models.Model):
+    score = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    user_id = models.IntegerField()
+    def __str__(self):
+        return str(self.score)
--- pacer_hw/settings.py
+++ pacer_hw/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pacer_app.apps.PacerAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
