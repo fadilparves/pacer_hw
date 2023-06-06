4235a8260b450d29e3ab6ea3279beb546351f3b1
Mohamad Fadil <fadil.parves@gmail.com>
2023-06-06 15:51:16 +0800

    added admin panel access

--- pacer_app/admin.py
+++ pacer_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Score
 
 # Register your models here.
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'score')
+    search_fields = ['user_id', 'score']
+
+admin.site.register(Score, ScoreAdmin)
