41733dfc17a1c1b48fbdc5f2479a250136e84ee5
Mohamad Fadil <fadil.parves@gmail.com>
2023-06-06 16:05:12 +0800

    update the settings, forgot to add .env

--- pacer_hw/settings.py
+++ pacer_hw/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +81,10 @@ WSGI_APPLICATION = 'pacer_hw.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "pacer_hw",
-        "USER": "mohamadfadil",
-        "PASSWORD": "",
-        "HOST": "localhost",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PW"),
+        "HOST": os.environ.get("DB_HOST"),
         "PORT": "5432",
     }
 }
