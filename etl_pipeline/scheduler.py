d6f09764d0450bdc9b0f99dc3d7646ce11203ee7
Mohamad Fadil <fadil.parves@gmail.com>
2023-06-25 10:35:57 +0800

    added etl pipeline

--- etl_pipeline/scheduler.py
+++ etl_pipeline/scheduler.py
@@ -1,12 +1,19 @@
 from scheduler.recurring import recurring_scheduler
-from etl_pipeline import etl_manager
+from pipeline import etl_manager
+from datetime import datetime, timedelta
 
 print("Scheduler started")
+
+current_datetime = datetime.now()
+
+time_diff = timedelta(minutes=5)
+
+future_datetime = current_datetime + time_diff
 
 # Run the etl_manager function every 5 minutes
 recurring_scheduler.add_job(target=etl_manager,
                             period_in_seconds=300,
-                            start="Jun 07 09:40:00 2022",
+                            start=future_datetime.strftime("%b %d %H:%M:%S %Y"),
                             tz="Asia/Kuala_Lumpur")
 
 recurring_scheduler.run()
